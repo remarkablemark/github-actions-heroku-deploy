e318b01869b785755ed63b064c020bd314c8ed71
Mark <mark@remarkablemark.org>
2021-03-12 17:53:43 -0500

    feat(index): create server that responds with "Hello World"

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
